09403a757bb0953b4d0ddf65c04ece6f6261e31c
Arpita <arpitaistic@gmail.com>
2026-01-02 12:55:24 +0530

    Naming Convention

---
+++ Naming Convention/main.java
@@ -0,0 +1,10 @@
+public class main {
+    public static void main (String [] args){
+        //Case Sensitive
+        //Can be Letters, Digits, Dollar Signs, or Underscores
+        //Must Begin with a letter, Dollar Sign ($), or Underscore (_)
+        //Cannnot use Java Keywords as Variable names
+        String myFullName = "Arpita Das";
+        System.out.println(myFullName);
+    }
+}
